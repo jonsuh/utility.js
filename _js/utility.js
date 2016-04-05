61e678120038003a3c545721c25c931fc78ad99c
Jonathan Suh <hello@jonsuh.com>
2016-04-05 15:00:22 -0500

    forEach.

--- _js/utility.js
+++ _js/utility.js
@@ -2,6 +2,31 @@ var Utility = (function() {
   "use strict";
 
   /**
+   * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
+   *
+   * @private
+   * @author Todd Motto
+   * @link https://github.com/toddmotto/foreach
+   * @param {Array|Object|NodeList} collection - Collection of items to iterate, could be an Array, Object or NodeList
+   * @callback requestCallback      callback   - Callback function for each iteration.
+   * @param {Array|Object|NodeList} scope=null - Object/NodeList/Array that forEach is iterating over, to use as the this value when executing callback.
+   * @returns {}
+   */
+  var forEach = function (collection, callback, scope) {
+    if (Object.prototype.toString.call(collection) === "[object Object]") {
+      for (var prop in collection) {
+        if (Object.prototype.hasOwnProperty.call(collection, prop)) {
+          callback.call(scope, collection[prop], prop, collection);
+        }
+      }
+    } else {
+      for (var i = 0, len = collection.length; i < len; i++) {
+        callback.call(scope, collection[i], i, collection);
+      }
+    }
+  };
+
+  /**
    * Tests whether a supplied variable is an Array.
    * http://stackoverflow.com/a/4775737/879081
    *
@@ -192,6 +217,7 @@ var Utility = (function() {
 
 
   return {
+    forEach: forEach,
     isArray: isArray,
     isBoolean: isBoolean,
     isDomElement: isDomElement,
